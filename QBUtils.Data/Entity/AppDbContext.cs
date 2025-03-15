6c07bc6245eb12a254b080b491d85a6f26928674
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-15 04:42:33 -0400

    Add project files.

---
+++ QBUtils.Data/Entity/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace QBUtils.Data;
+
+internal class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
+
+internal class AppDbContextDesignTimeFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddJsonFile($"appsettings.{envName}.json", optional: true)
+            .AddEnvironmentVariables()
+            .AddCommandLine(args)
+            .Build();
+
+        var connString = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Unable to get connection string.");
+
+        var optBuilder = new DbContextOptionsBuilder()
+            .UseSqlServer(connString);
+
+        return new AppDbContext(optBuilder.Options);
+    }
+}
---
+++ QBUtils.Data/Services/RegisterDataServices.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QBUtils.Data.Services;
+
+public static class RegisterDataServices
+{
+    public static IServiceCollection AddDataServices(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<AppDbContext>(o => o.UseSqlServer(connectionString))
+            .AddDefaultIdentity<AppUser>()
+            .AddRoles<IdentityRole>()
+            .AddDefaultTokenProviders()
+            .AddEntityFrameworkStores<AppDbContext>();
+
+        services.AddMediatR(config => config.RegisterServicesFromAssembly(System.Reflection.Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
