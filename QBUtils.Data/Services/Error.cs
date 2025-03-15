887d37a2cd8ad6ef3cf97fce95070ba644d162a6
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-15 10:51:58 -0400

    Adds additional error types.

--- QBUtils.Data/Services/Error.cs
+++ QBUtils.Data/Services/Error.cs
@@ -15,6 +15,9 @@ public abstract class Error
     }
 
     public static BadRequestError BadRequestError() => new BadRequestError();
+    public static ForbiddenError ForbiddenError() => new ForbiddenError();
+    public static NotFoundError NotFoundError() => new NotFoundError();
+    public static ServerError ServerError() => new ServerError();
 }
 
 public class BadRequestError : Error
@@ -34,6 +37,60 @@ public class BadRequestError : Error
     public BadRequestError(string errorMessage) : base()
     {
         StatusCode = 400;
+        StatusMessage = errorMessage;
+    }
+}
+
+public class ForbiddenError : Error
+{
+    [SetsRequiredMembers]
+    public ForbiddenError() : this("Forbidden")
+    {
+
+    }
+
+    [SetsRequiredMembers]
+    public ForbiddenError(string errorMessage) : base()
+    {
+        StatusCode = 403;
+        StatusMessage = errorMessage;
+    }
+}
+
+public class NotFoundError : Error
+{
+    [SetsRequiredMembers]
+    public NotFoundError() : this("Not Found")
+    {
+
+    }
+
+    [SetsRequiredMembers]
+    public NotFoundError(string key, string errorMessage) : this()
+    {
+        ModelState.TryAddModelError(key, errorMessage);
+    }
+
+    [SetsRequiredMembers]
+    public NotFoundError(string errorMessage) : base()
+    {
+        StatusCode = 404;
+        StatusMessage = errorMessage;
+    }
+}
+
+public class ServerError : Error
+{
+    [SetsRequiredMembers]
+    public ServerError() : this("Server Error")
+    {
+
+    }
+
+    [SetsRequiredMembers]
+    public ServerError(string errorMessage) : base()
+    {
+        StatusCode = 500;
         StatusMessage = errorMessage;
     }
 }
