dffedc9f0874fe9d45f8cab2ceecd8258beba92c
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-15 04:59:17 -0400

    Adds Error and Result classes for CQRS responses.

---
+++ QBUtils.Data/Services/Error.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Diagnostics.CodeAnalysis;
+
+namespace QBUtils.Data.Services;
+
+public abstract class Error
+{
+    public int StatusCode { get; set; }
+    public required string StatusMessage { get; set; }
+    public ModelStateDictionary ModelState { get; }
+
+    public Error()
+    {
+        ModelState = new ModelStateDictionary();
+    }
+
+    public static BadRequestError BadRequestError() => new BadRequestError();
+}
+
+public class BadRequestError : Error
+{
+    [SetsRequiredMembers]
+    public BadRequestError() : this("Bad Request")
+    {
+    }
+
+    [SetsRequiredMembers]
+    public BadRequestError(string key, string errorMessage) : this()
+    {
+        ModelState.TryAddModelError(key, errorMessage);
+    }
+
+    [SetsRequiredMembers]
+    public BadRequestError(string errorMessage) : base()
+    {
+        StatusCode = 400;
+        StatusMessage = errorMessage;
+    }
+}
---
+++ QBUtils.Data/Services/Result.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace QBUtils.Data.Services;
+
+public class Result
+{
+    public int StatusCode { get; set; }
+    public bool WasSuccessful => StatusCode >= 299 && StatusCode <= 299;
+    public required string StatusMessage { get; init; }
+    public List<Error>? Errors { get; set; }
+    public bool HasErrors => Errors != null && Errors.Any();
+
+    [SetsRequiredMembers]
+    public Result(int statusCode, string statusMessage)
+    {
+        StatusCode = statusCode;
+        StatusMessage = statusMessage;
+
+    }
+
+    [SetsRequiredMembers]
+    public Result(int statusCode, string statusMessage, Error error) : this(statusCode, statusMessage)
+    {
+        Errors = [error];
+    }
+
+    public static implicit operator Result(Error error) => new Result(error.StatusCode, error.StatusMessage);
+}
+
+public class Result<T> : Result
+{
+    private T? _Value;
+    public T? Value => WasSuccessful ? _Value : throw new InvalidOperationException("Unable to get Value of unsuccessful Result.");
+
+    [SetsRequiredMembers]
+    public Result(int statusCode, string statusMessage, T? value) : base(statusCode, statusMessage)
+    {
+        _Value = value;
+    }
+
+    public static implicit operator Result<T>(T value) => new Result<T>(200, "OK", value);
+    public static implicit operator Result<T>(Error error) => new Result<T>(error.StatusCode, error.StatusMessage, default);
+}
