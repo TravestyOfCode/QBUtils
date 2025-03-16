1525481c5f4c18a4d4726c166f9f025e8b43d959
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-15 20:10:04 -0400

    Adds get all query.

---
+++ QBUtils.Data/Services/SQLConnection/Queries/GetSQLConnectionsByUser.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace QBUtils.Data.Services.SQLConnection;
+
+public class GetSQLConnectionsByUser : IRequest<Result<List<SQLConnectionModel>>>
+{
+    public required string UserId { get; set; }
+}
+
+internal class GetSQLConnectionsByUserHandler : IRequestHandler<GetSQLConnectionsByUser, Result<List<SQLConnectionModel>>>
+{
+    private readonly AppDbContext _dbContext;
+
+    private readonly ILogger<GetSQLConnectionsByUserHandler> _logger;
+
+    public GetSQLConnectionsByUserHandler(AppDbContext dbContext, ILogger<GetSQLConnectionsByUserHandler> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    public async Task<Result<List<SQLConnectionModel>>> Handle(GetSQLConnectionsByUser request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await _dbContext.SQLConnections
+                .Where(p => p.UserId.Equals(request.UserId))
+                .ProjectToModel()
+                .ToListAsync(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexected error handling request: {request}", request);
+
+            return Error.ServerError();
+        }
+    }
+}
